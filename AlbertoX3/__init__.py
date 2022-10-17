f650eeff003cdafff03e83f317718461a2b0ccad
AlbertUnruh <AlbertUnruh@pm.me>
2022-10-17 12:49:03 +0200

    Initial Commit

---
+++ AlbertoX3/__init__.py
@@ -0,0 +1,5 @@
+__author__ = "AlbertUnruh"
+__version__ = "3.0.0a"
+__license__ = "GNU AGPLv3"
+__copyright__ = f"Copyright 2022-present (c) {__author__} - Alberto-X3"
+__url__ = "https://github.com/Alberto-X3/Alberto-X3-V3"
