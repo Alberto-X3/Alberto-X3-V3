cf5e7db56c29ff4cdc063f62244b5dd864d440f9
AlbertUnruh <AlbertUnruh@pm.me>
2022-10-19 11:43:47 +0200

    add PrimitiveExtension to misc.py

--- AlbertoX3/misc.py
+++ AlbertoX3/misc.py
@@ -1,5 +1,21 @@
-__all__ = ("FormatStr",)
+__all__ = (
+    "FormatStr",
+    "PrimitiveExtension",
+)
 
 
 class FormatStr(str):
     __call__ = str.format
+
+
+class PrimitiveExtension:
+    def __init__(self, name, package, path):
+        self.name = name
+        self.package = package
+        self.path = path
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} ({self.name!r} at {self.path})>"
+
+    def __str__(self):
+        return f"<{self.__class__.__name__} {self.name!r}>"
--- AlbertoX3/misc.pyi
+++ AlbertoX3/misc.pyi
@@ -1,4 +1,13 @@
+from pathlib import Path
 from typing import Callable
 
 class FormatStr(str):
     __call__: Callable[..., str]  # see str.format
+
+class PrimitiveExtension:
+    name: str
+    package: str
+    path: Path
+    def __init__(self, name: str, package: str, path: Path): ...
+    def __repr__(self) -> str: ...
+    def __str__(self) -> str: ...
