6d65a044706a45b2db811e545d7c599d07f0d832
AlbertUnruh <AlbertUnruh@pm.me>
2022-10-20 12:16:21 +0200

    move ``misc.pyi`` into ``misc.py``

--- AlbertoX3/misc.py
+++ AlbertoX3/misc.py
@@ -4,18 +4,26 @@ __all__ = (
 )
 
 
+from pathlib import Path
+from typing import Callable
+
+
 class FormatStr(str):
-    __call__ = str.format
+    __call__: Callable[..., str] = str.format
 
 
 class PrimitiveExtension:
-    def __init__(self, name, package, path):
+    name: str
+    package: str
+    path: Path
+
+    def __init__(self, name: str, package: str, path: Path):
         self.name = name
         self.package = package
         self.path = path
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"<{self.__class__.__name__} ({self.name!r} at {self.path})>"
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"<{self.__class__.__name__} {self.name!r}>"
