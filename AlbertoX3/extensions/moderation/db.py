76145e55d619e0cd0fa62460e7375ebe13287d75
AlbertUnruh <AlbertUnruh@pm.me>
2022-10-26 18:15:00 +0200

    every feature from #7 has a structure
    Notes:
    - connected to database
    - no effects on Discord (doesn't actually ban etc)

--- AlbertoX3/extensions/moderation/db.py
+++ AlbertoX3/extensions/moderation/db.py
@@ -1,6 +1,10 @@
 __all__ = (
     "BanModel",
+    "UnbanModel",
     "KickModel",
+    "MuteModel",
+    "UnmuteModel",
+    "DeleteModel",
 )
 
 
@@ -32,6 +36,27 @@ class BanModel(Base):
         )
 
 
+class UnbanModel(Base):
+    __tablename__ = "unban"
+
+    id: Column | int = Column(Integer, primary_key=True, unique=True, autoincrement=True, nullable=False)
+    member: Column | int = Column(BigInteger, nullable=False)
+    executor: Column | int = Column(BigInteger, nullable=False)
+    timestamp: Column | datetime = Column(UTCDatetime, nullable=False)
+    reason: Column | str = Column(Text(128), nullable=False)
+
+    @staticmethod
+    async def add(member: int, executor: int, reason: str) -> "UnbanModel":
+        return await db.add(
+            UnbanModel(
+                member=member,
+                executor=executor,
+                timestamp=datetime.utcnow(),
+                reason=reason,
+            )
+        )
+
+
 class KickModel(Base):
     __tablename__ = "kick"
 
@@ -49,5 +74,72 @@ class KickModel(Base):
                 executor=executor,
                 timestamp=datetime.utcnow(),
                 reason=reason,
+            )
+        )
+
+
+class MuteModel(Base):
+    __tablename__ = "mute"
+
+    id: Column | int = Column(Integer, primary_key=True, unique=True, autoincrement=True, nullable=False)
+    member: Column | int = Column(BigInteger, nullable=False)
+    executor: Column | int = Column(BigInteger, nullable=False)
+    timestamp: Column | datetime = Column(UTCDatetime, nullable=False)
+    reason: Column | str = Column(Text(128), nullable=False)
+    until: Column | datetime = Column(UTCDatetime, nullable=True)
+
+    @staticmethod
+    async def add(member: int, executor: int, reason: str, until: datetime | None) -> "MuteModel":
+        return await db.add(
+            MuteModel(
+                member=member,
+                executor=executor,
+                timestamp=datetime.utcnow(),
+                reason=reason,
+                until=until,
+            )
+        )
+
+
+class UnmuteModel(Base):
+    __tablename__ = "unmute"
+
+    id: Column | int = Column(Integer, primary_key=True, unique=True, autoincrement=True, nullable=False)
+    member: Column | int = Column(BigInteger, nullable=False)
+    executor: Column | int = Column(BigInteger, nullable=False)
+    timestamp: Column | datetime = Column(UTCDatetime, nullable=False)
+    reason: Column | str = Column(Text(128), nullable=False)
+
+    @staticmethod
+    async def add(member: int, executor: int, reason: str) -> "UnmuteModel":
+        return await db.add(
+            UnmuteModel(
+                member=member,
+                executor=executor,
+                timestamp=datetime.utcnow(),
+                reason=reason,
+            )
+        )
+
+
+class DeleteModel(Base):
+    __tablename__ = "delete"
+
+    id: Column | int = Column(Integer, primary_key=True, unique=True, autoincrement=True, nullable=False)
+    amount: Column | int = Column(Integer, nullable=False)
+    channel: Column | int = Column(BigInteger, nullable=False)
+    executor: Column | int = Column(BigInteger, nullable=False)
+    timestamp: Column | datetime = Column(UTCDatetime, nullable=False)
+    user: Column | int = Column(BigInteger, nullable=True)
+
+    @staticmethod
+    async def add(amount: int, channel: int, executor: int, user: int | None) -> "DeleteModel":
+        return await db.add(
+            DeleteModel(
+                amount=amount,
+                channel=channel,
+                executor=executor,
+                timestamp=datetime.utcnow(),
+                user=user,
             )
         )
--- AlbertoX3/extensions/moderation/ext.py
+++ AlbertoX3/extensions/moderation/ext.py
@@ -5,7 +5,7 @@ from AlbertoX3 import get_logger
 from datetime import datetime, timezone
 from datetimeparser import parse
 from naff import Extension, InteractionContext, Member, OptionTypes, SlashCommandOption, slash_command
-from .db import BanModel, KickModel
+from .db import BanModel, UnbanModel, KickModel, MuteModel, UnmuteModel, DeleteModel
 
 
 logger = get_logger(__name__)
@@ -14,18 +14,18 @@ logger = get_logger(__name__)
 class Moderation(Extension):
     @slash_command(
         "ban",
-        description="Wields the banhammer",
+        description="Wields the ban-hammer",
         options=[
             SlashCommandOption(
                 name="user",
-                description="The user to hit with the banhammer",
+                description="The user to hit with the ban-hammer",
                 required=True,
                 autocomplete=False,
                 type=OptionTypes.USER,
             ),
             SlashCommandOption(
                 name="reason",
-                description="Why you want to wield the banhammer",
+                description="Why you want to wield the ban-hammer",
                 required=False,
                 autocomplete=False,
                 type=OptionTypes.STRING,
@@ -40,7 +40,9 @@ class Moderation(Extension):
             ),
         ],
     )
-    async def ban(self, ctx: InteractionContext, *, user: Member, reason: str | None = None, duration: str | None):
+    async def ban(
+        self, ctx: InteractionContext, *, user: Member, reason: str | None = None, duration: str | None = None
+    ):
         if reason is None:
             reason = "NO REASON -- REPLACE THIS WITH TRANSLATION"
 
@@ -70,8 +72,45 @@ class Moderation(Extension):
         await ctx.send(f"Banned ``{user.tag}`` with reason ``{reason}`` {info}")
 
     @slash_command(
+        "unban",
+        description="Takes away the ban-hammer",
+        options=[
+            SlashCommandOption(
+                name="user",
+                description="The user to take away the ban-hammer",
+                required=True,
+                autocomplete=False,
+                type=OptionTypes.USER,
+            ),
+            SlashCommandOption(
+                name="reason",
+                description="Why you want to take away the ban-hammer",
+                required=False,
+                autocomplete=False,
+                type=OptionTypes.STRING,
+                max_length=128,
+            ),
+        ],
+    )
+    async def unban(self, ctx: InteractionContext, *, user: Member, reason: str | None = None):
+        if reason is None:
+            reason = "NO REASON -- REPLACE THIS WITH TRANSLATION"
+
+        logger.warning(f"{__name__}.Moderation.unban has no actual functionality!")
+        from AlbertoX3.database import db_context
+
+        async with db_context():
+            await UnbanModel.add(
+                member=user.id,
+                executor=ctx.author.id,
+                reason=reason,
+            )
+
+        await ctx.send(f"Unbanned ``{user.tag}`` with reason ``{reason}``")
+
+    @slash_command(
         "kick",
-        description="Wields the lame kick-stick",
+        description="Wields the kick-stick",
         options=[
             SlashCommandOption(
                 name="user",
@@ -105,3 +144,140 @@ class Moderation(Extension):
             )
 
         await ctx.send(f"Kicked ``{user.tag}`` with reason ``{reason}``")
+
+    @slash_command(
+        "mute",
+        description="Throws the mute-axe",
+        options=[
+            SlashCommandOption(
+                name="user",
+                description="The user to hit with the mute-axe",
+                required=True,
+                autocomplete=False,
+                type=OptionTypes.USER,
+            ),
+            SlashCommandOption(
+                name="reason",
+                description="Why you want to wield the mute-axe",
+                required=False,
+                autocomplete=False,
+                type=OptionTypes.STRING,
+                max_length=128,
+            ),
+            SlashCommandOption(
+                name="duration",
+                description="Until when the user shall never be heard (ISO/english description)",
+                required=False,
+                autocomplete=False,
+                type=OptionTypes.STRING,
+            ),
+        ],
+    )
+    async def mute(
+        self, ctx: InteractionContext, *, user: Member, reason: str | None = None, duration: str | None = None
+    ):
+        if reason is None:
+            reason = "NO REASON -- REPLACE THIS WITH TRANSLATION"
+
+        until: datetime | None = None
+        if duration is not None:
+            parsed = parse(duration, "utc")
+            utc = datetime.utcnow()  # don't use utils.get_utcnow since parse doesn't set tzinfo
+            if parsed > utc:  # in the future
+                until = parsed.replace(tzinfo=timezone.utc)
+
+        logger.warning(f"{__name__}.Moderation.mute has no actual functionality!")
+        from AlbertoX3.database import db_context
+
+        async with db_context():
+            await MuteModel.add(
+                member=user.id,
+                executor=ctx.author.id,
+                reason=reason,
+                until=until,
+            )
+
+        if until:
+            info = f"(until <t:{int(until.timestamp())}:f>)"
+        else:
+            info = "(permanently)"
+
+        await ctx.send(f"Muted ``{user.tag}`` with reason ``{reason}`` {info}")
+
+    @slash_command(
+        "unmute",
+        description="Takes away the mute-axe",
+        options=[
+            SlashCommandOption(
+                name="user",
+                description="The user to take away the mute-axe",
+                required=True,
+                autocomplete=False,
+                type=OptionTypes.USER,
+            ),
+            SlashCommandOption(
+                name="reason",
+                description="Why you want to take away the mute-axe",
+                required=False,
+                autocomplete=False,
+                type=OptionTypes.STRING,
+                max_length=128,
+            ),
+        ],
+    )
+    async def unmute(self, ctx: InteractionContext, *, user: Member, reason: str | None = None):
+        if reason is None:
+            reason = "NO REASON -- REPLACE THIS WITH TRANSLATION"
+
+        logger.warning(f"{__name__}.Moderation.unmute has no actual functionality!")
+        from AlbertoX3.database import db_context
+
+        async with db_context():
+            await UnmuteModel.add(
+                member=user.id,
+                executor=ctx.author.id,
+                reason=reason,
+            )
+
+        await ctx.send(f"Unmuted ``{user.tag}`` with reason ``{reason}``")
+
+    @slash_command(
+        "delete",
+        description="Takes out the eraser",
+        options=[
+            SlashCommandOption(
+                name="amount",
+                description="How many messages to wipe out",
+                required=True,
+                autocomplete=False,
+                type=OptionTypes.INTEGER,
+                min_value=1,
+                max_value=128,
+            ),
+            SlashCommandOption(
+                name="user",
+                description="The user to wipe out from chat",
+                required=False,
+                autocomplete=False,
+                type=OptionTypes.USER,
+            ),
+        ],
+    )
+    async def delete(self, ctx: InteractionContext, *, amount: int, user: Member | None = None):
+        logger.warning(f"{__name__}.Moderation.delete has no actual functionality!")
+        from AlbertoX3.database import db_context
+
+        async with db_context():
+            await DeleteModel.add(
+                amount=amount,
+                channel=ctx.channel.id,
+                executor=ctx.author.id,
+                user=user.id,
+            )
+
+        if user is not None:
+            info = user.tag
+        else:
+            info = "@everyone"
+
+        await ctx.send(f"Deleted ``{amount}`` messages from ``{info}``")
