ea83b585448cbf9996e3a4f2f4eac87c7cc39c57
AlbertUnruh <AlbertUnruh@pm.me>
2022-12-14 14:35:03 +0100

    add more participants

--- extensions/role_play/db.py
+++ extensions/role_play/db.py
@@ -4,6 +4,7 @@ __all__ = ("FileCaseModel",)
 from AlbertoX3.constants import MISSING
 from AlbertoX3.database import Base, UTCDatetime, db
 from datetime import datetime
+from naff.client.const import EMBED_FIELD_VALUE_LENGTH
 from sqlalchemy import Column, Integer, BigInteger, Text, func
 
 
@@ -14,22 +15,46 @@ class FileCaseModel(Base):
     author: Column | int = Column(BigInteger, nullable=False)
     created: Column | datetime = Column(UTCDatetime, nullable=False)
     last_edited: Column | datetime = Column(UTCDatetime, nullable=False)
+    status: Column | int = Column(Integer, nullable=False)
     judge: Column | int = Column(BigInteger, nullable=False)
+    lay_judge: Column | int | None = Column(BigInteger, nullable=True)
     complainant: Column | int = Column(BigInteger, nullable=False)
+    complainant_lawyer: Column | int | None = Column(BigInteger, nullable=True)
     defendant: Column | int = Column(BigInteger, nullable=False)
-    appeal: Column | str = Column(Text(512), nullable=False)
+    defendant_lawyer: Column | int | None = Column(BigInteger, nullable=True)
+    witness: Column | int | None = Column(BigInteger, nullable=True)
+    expert: Column | int | None = Column(BigInteger, nullable=True)
+    accusation: Column | str = Column(Text(EMBED_FIELD_VALUE_LENGTH), nullable=False)
 
     @staticmethod
-    async def create(author: int, judge: int, complainant: int, defendant: int, appeal: str) -> "FileCaseModel":
+    async def create(
+        author: int,
+        status: int,
+        judge: int,
+        lay_judge: int | None,
+        complainant: int,
+        complainant_lawyer: int | None,
+        defendant: int,
+        defendant_lawyer: int | None,
+        witness: int | None,
+        expert: int | None,
+        accusation: str,
+    ) -> "FileCaseModel":
         return await db.add(
             FileCaseModel(
                 author=author,
                 created=(utcnow := datetime.utcnow()),
                 last_edited=utcnow,
+                status=status,
                 judge=judge,
+                lay_judge=lay_judge,
                 complainant=complainant,
+                complainant_lawyer=complainant_lawyer,
                 defendant=defendant,
-                appeal=appeal,
+                defendant_lawyer=defendant_lawyer,
+                witness=witness,
+                expert=expert,
+                accusation=accusation,
             )
         )
 
@@ -42,15 +67,25 @@ class FileCaseModel(Base):
         return await db.get(FileCaseModel, FileCaseModel.last_edited == func.max(FileCaseModel.last_edited))
 
     async def edit(
-        self, *, judge: int = MISSING, complainant: int = MISSING, defendant: int = MISSING, appeal: str = MISSING
+        self,
+        *,
+        status: int = MISSING,
+        judge: int = MISSING,
+        lay_judge: int | None = MISSING,
+        complainant: int = MISSING,
+        complainant_lawyer: int | None = MISSING,
+        defendant: int = MISSING,
+        defendant_lawyer: int | None = MISSING,
+        witness: int | None = MISSING,
+        expert: int | None = MISSING,
+        accusation: str = MISSING,
     ) -> "FileCaseModel":
-        if judge is not MISSING:
-            self.judge = judge
-        if complainant is not MISSING:
-            self.complainant = complainant
-        if defendant is not MISSING:
-            self.defendant = defendant
-        if appeal is not MISSING:
-            self.appeal = appeal
+        args = locals()
+        args.pop("self")
+
+        for name, value in args.items():
+            if value is not MISSING:
+                setattr(self, name, value)
+
         self.last_edited = datetime.utcnow()
         return self
--- extensions/role_play/file_case.py
+++ extensions/role_play/file_case.py
@@ -12,6 +12,13 @@ tg: TranslationNamespace = t.g
 t: TranslationNamespace = t.role_play.file_case  # real type: TranslationDict
 
 
+_CASE_STATUS: dict[int, str] = {
+    0: "open",
+    1: "active",
+    2: "closed",
+}
+
+
 class FileCase(Extension):
     @slash_command(
         "file-case",
@@ -19,35 +26,79 @@ class FileCase(Extension):
     )
     async def fc_info(self, ctx: InteractionContext):
         case = await FileCaseModel.get_last_recent_updated()
-        embed = Embed(
-            title=t.last_recent_title(id=case.id),
+        title = t.last_recent_title(id=case.id)
+        embed = self.get_case_embed(title, case)
+        await ctx.send(embeds=[embed])
+
+    @staticmethod
+    def get_case_embed(title: str, case: FileCaseModel) -> Embed:
+        return Embed(
+            title=title,
             description=t.description(
+                status=getattr(t.status, _CASE_STATUS[case.status]),
                 created=int(case.created.timestamp()),
                 last_edited=int(case.last_edited.timestamp()),
                 author=case.author,
             ),
             fields=[
+                # row 1
                 EmbedField(  # Judge
                     name=t.case.judge.name,
                     value=t.case.judge.value(judge=case.judge),
                     inline=True,
                 ),
+                EmbedField(  # Lay Judge
+                    name=t.case.lay_judge.name,
+                    value=t.case.lay_judge.value(lay_judge=case.lay_judge, cnt=case.lay_judge is not None),
+                    inline=True,
+                ),
+                EmbedField("\u200b", "\u200b", True),
+                # row 2
                 EmbedField(  # Complainant
                     name=t.case.complainant.name,
                     value=t.case.complainant.value(complainant=case.complainant),
                     inline=True,
                 ),
+                EmbedField(  # Complainant Lawyer
+                    name=t.case.complainant_lawyer.name,
+                    value=t.case.complainant_lawyer.value(
+                        complainant_lawyer=case.complainant_lawyer, cnt=case.complainant_lawyer is not None
+                    ),
+                    inline=True,
+                ),
+                EmbedField("\u200b", "\u200b", True),
+                # row 3
                 EmbedField(  # Defendant
                     name=t.case.defendant.name,
                     value=t.case.defendant.value(defendant=case.defendant),
                     inline=True,
                 ),
-                EmbedField(  # Appeal
-                    name=t.case.appeal.name,
-                    value=t.case.appeal.value(appeal=case.appeal),
+                EmbedField(  # Defendant Lawyer
+                    name=t.case.defendant_lawyer.name,
+                    value=t.case.defendant_lawyer.value(
+                        defendant_lawyer=case.defendant_lawyer, cnt=case.defendant_lawyer is not None
+                    ),
+                    inline=True,
+                ),
+                EmbedField("\u200b", "\u200b", True),
+                # row 4
+                EmbedField(  # Witness
+                    name=t.case.witness.name,
+                    value=t.case.witness.value(witness=case.witness, cnt=case.witness is not None),
+                    inline=True,
+                ),
+                EmbedField(  # Expert
+                    name=t.case.expert.name,
+                    value=t.case.expert.value(expert=case.expert, cnt=case.expert is not None),
+                    inline=True,
+                ),
+                EmbedField("\u200b", "\u200b", True),
+                # row 5
+                EmbedField(  # Accusation
+                    name=t.case.accusation.name,
+                    value=t.case.accusation.value(accusation=case.accusation),
                     inline=False,
                 ),
             ],
             color=Colors.file_case,
         )
-        await ctx.send(embeds=[embed])
