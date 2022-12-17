08f58ea762c80670a80b39fddd562273c8a52d28
AlbertUnruh <73029826+AlbertUnruh@users.noreply.github.com>
2022-12-17 17:09:03 +0100

    fix logic

--- AlbertoX3.sh
+++ AlbertoX3.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-if [ "$0" = --skip ];
+if [ "$1" == "--skip" ];
 then
+  echo [AlbertoX3] skipping requirements
+else
   echo [AlbertoX3] uninstalling pip environment
   pip freeze | grep -F -v "naff @" | xargs pip uninstall naff -y
 
   echo [AlbertoX3] installing requirements.txt
   pip install -U -r requirements.txt
-else
-  echo [AlbertoX3] skipping requirements
 fi
 
 echo
